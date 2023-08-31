ac72fe7866fd4b39e2e52bf7f92c6dbeb407e848
Lukasz Chwiedosik <lukasz.chwiedosik@gmail.com>
2023-08-31 13:52:44 +0200

    Feat: handled conditions reactive form, when form is valid data template for this form is save to localstorage

--- src/app/pages/definition-page/conditionsform/conditionsform.component.ts
+++ src/app/pages/definition-page/conditionsform/conditionsform.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-conditionsform',
@@ -7,7 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConditionsformComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _formBuilder: FormBuilder) {
+    this.conditionsForm.valueChanges.subscribe(data => {
+      console.log(data)
+      if (data.portal !== '' || data.portal !== null) {
+        this.disabledSelectState = false;
+      } else {
+        this.disabledSelectState = true;
+      }
+    });
+
+    this.conditionsForm.statusChanges.subscribe(status => {
+      const data = this.conditionsForm.value
+      if (status === 'VALID') {
+        localStorage.setItem('conditionsForm', JSON.stringify(data));
+      }
+    })
+  }
+
+  disabledSelectState: boolean = true
+
+  public conditionsForm = this._formBuilder.group({
+    portal: [null, [Validators.required]],
+    type: [null, [Validators.required]],
+    benefitAmount: [null],
+    startDate: [null, [Validators.required]],
+    endDate: [null],
+    bussinessRadio: ['Bussiness conditions'],
+    connectPromotions: [true],
+    backPromotion: [false]
+  });
 
   ngOnInit(): void {
   }

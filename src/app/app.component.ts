1caa3e58eb732efa599756df58fd0997643496ba
Lukasz Chwiedosik <lukasz.chwiedosik@gmail.com>
2023-08-31 21:03:44 +0200

    Feat: on load app the item with empty array will be create

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   constructor() { }
 
+  ngOnInit(): void {
+    localStorage.setItem('definitionDataArray', JSON.stringify([]));
+  }
 }
